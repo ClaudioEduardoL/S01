5e88ee2e4f8b956c22d3345c6ee641809d02cfd1
ClaudioEduardoL <111525442+ClaudioEduardoL@users.noreply.github.com>
2025-09-11 21:02:48 -0300

    Add files via upload

---
+++ atv4.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func verificarLogin(usuario, senha string) bool {
+	if usuario == "senha" && senha == "admin" {
+		return true
+	}
+	return false
+}
+
+func main() {
+	for {
+		var usuario, senha string
+
+		fmt.Print("Usuário: ")
+		fmt.Scanln(&usuario)
+
+		fmt.Print("Senha: ")
+		fmt.Scanln(&senha)
+
+		if verificarLogin(usuario, senha) {
+			fmt.Println("Login bem-sucedido!")
+			break
+		} else {
+			fmt.Println("Usuário ou senha incorretos. Tente novamente.")
+		}
+	}
+}
